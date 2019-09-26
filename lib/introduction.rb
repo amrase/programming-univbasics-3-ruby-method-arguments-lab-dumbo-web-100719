0b0d58ec03fde4185cbaddbb8701635b71251de9
Amra Sezairi <amra.sezairi@hotmail.com>
2019-09-26 02:03:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,10 @@ def introduction(name="Josh")
 end  
 
 
+def introduction_with_language(name="Dan",language="Ember.js")
+  puts "Hi,my name is #{name} and I am learning to program #{language}"
+end
+
 
 
 
